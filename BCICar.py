640e675ae67f7d58fa2a1997a9c9b513dc2a3e30
Wilson <wilsonjwtran@gmail.com>
2018-03-22 23:24:41 -0700

    Fixed formatting

--- BCICar.py
+++ BCICar.py
@@ -1,37 +1,17 @@
-
-# coding: utf-8
-
-# # BCI Car
-
-# In[1]:
-
-
 from NeuroPy import NeuroPy
 import numpy as np
 import serial
 import time
 
-
-# In[34]:
-
-
 # Instantiation of Mindwave Mobile 
 mindwave = NeuroPy(port="COM12", baudRate=57600)
 mindwave.start()
-
-
-# In[35]:
-
 
 # Instantiation of Bluetooth to Arduino and flush serial
 HC06_port = "COM15"
 ser = serial.Serial(port=HC06_port, baudrate=9600)
 ser.reset_input_buffer()
 ser.reset_output_buffer()
-
-
-# In[37]:
-
 
 # Instantiation of maximum and minimum speeds
 maxSpeed = 240
@@ -53,12 +33,8 @@ while True:
         ser.write('0\n')
 
     time.sleep(1)
-
-
-# In[33]:
-
-
+    
 # Close and terminate existing ports
-ser.close()
-mindwave.stop()
+# ser.close()
+# mindwave.stop()
 
